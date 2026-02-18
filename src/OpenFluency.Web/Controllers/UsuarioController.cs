1c33cb8c13d6ac45ce10b14262c7fd82da0f3da1
voishev <dimitryjunior.dev@gmail.com>
2026-02-18 17:10:26 -0300

    Criando a tela de login

---
+++ src/OpenFluency.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OpenFluency.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
